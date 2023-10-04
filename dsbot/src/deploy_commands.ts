6cf7c13832189f5b507f0677375cf3e079ffb2c1
SignorB <signorb@signorb.it>
2023-10-04 08:52:10 +0000

    indented + cleared env importing

--- dsbot/src/deploy_commands.ts
+++ dsbot/src/deploy_commands.ts
@@ -1,32 +1,31 @@
-import {REST, Routes, RESTPostAPIApplicationCommandsJSONBody} from 'discord.js'
-import {commands} from './commands/exporter'
-import * as dotenv from 'dotenv'
-dotenv.config()
+import { REST, Routes, RESTPostAPIApplicationCommandsJSONBody } from 'discord.js'
+import { commands } from './commands/exporter'
+
 const token = process.env.TOKEN || "error"
 const clientId = process.env.CLIENT_ID || "error"
 
-const commandJSONs : RESTPostAPIApplicationCommandsJSONBody[] =
-commands.map(cmnd => cmnd.data.toJSON())
+const commandJSONs: RESTPostAPIApplicationCommandsJSONBody[] =
+	commands.map(cmnd => cmnd.data.toJSON())
 
 interface pseudoData {
-length : number
+	length: number
 }
 
 const rest = new REST().setToken(token);
 
 (async () => {
-try {
-console.log(`Started refreshing ${commandJSONs.length} application (/) commands.`);
+	try {
+		console.log(`Started refreshing ${commandJSONs.length} application (/) commands.`);
 
-// The put method is used to fully refresh all commands in the guild with the current set
-	const data = (await rest.put(
-		Routes.applicationCommands(clientId),
-		{ body: commandJSONs },
-	)) as pseudoData;
+		// The put method is used to fully refresh all commands in the guild with the current set
+		const data = (await rest.put(
+			Routes.applicationCommands(clientId),
+			{ body: commandJSONs },
+		)) as pseudoData;
 
-	console.log(`Successfully reloaded ${data.length} application (/) commands.`);
-} catch (error) {
-	// And of course, make sure you catch and log any errors!
-	console.error(error);
-}
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	} catch (error) {
+		// And of course, make sure you catch and log any errors!
+		console.error(error);
+	}
 })();
