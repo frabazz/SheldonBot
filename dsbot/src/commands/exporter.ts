83e7b41ab019789fba395c0ead8d672917d3c867
SignorB <signorb@signorb.it>
2023-10-18 13:40:15 +0000

    cleared exported path

--- dsbot/src/commands/exporter.ts
+++
@@ -1,9 +0,0 @@
-import {SlashCommand} from '../types'
-import {Answer} from './Answer'
-import {Init} from './Init'
-import {Upload} from './Upload'
-export const commands : SlashCommand[] = [
-  Answer,
-  Init,
-  Upload,
-]
